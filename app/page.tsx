50545778219f08f3cefe74d04c9bf5a1c94b7716
Le Hoang Lam <lhlam2515@gmail.com>
2025-07-23 20:54:28 +0700

    Set up prettier and shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-primary-100">
+    <main className="bg-primary-100 flex min-h-screen items-center justify-center">
       <h1 className="text-primary-500 text-2xl font-bold">Hello World!</h1>
     </main>
   );
