ba21eb72ad7eb3df2cc6890ee507400a79606e31
Le Hoang Lam <lhlam2515@gmail.com>
2025-07-24 22:58:13 +0700

    Add background color to sidebar component

--- components/navigation/Sidebar.tsx
+++ components/navigation/Sidebar.tsx
@@ -3,7 +3,7 @@ import NavLinks from "./navbar/NavLinks";
 
 const Sidebar = () => {
   return (
-    <section className="border-secondary-500 sticky top-0 left-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-24 max-sm:hidden lg:w-[266px]">
+    <section className="border-secondary-500 bg-primary-100 sticky top-0 left-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-24 max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
       </div>
