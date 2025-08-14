f8c587c2518404189028b15e404015cc157ba03e
Le Hoang Lam <lhlam2515@gmail.com>
2025-08-14 20:32:48 +0700

    refactor: remove push token functionality from user model and actions

--- database/user.model.ts
+++ database/user.model.ts
@@ -8,7 +8,6 @@ export interface IUser {
   phone?: string;
   cardNumber?: string;
   cardExpiry?: string;
-  pushToken?: string;
 }
 
 export interface IUserDoc extends IUser, Document {}
@@ -21,7 +20,6 @@ const UserSchema = new Schema<IUser>(
     phone: { type: String, default: null },
     cardNumber: { type: String, default: null },
     cardExpiry: { type: String, default: null },
-    pushToken: { type: String, default: null },
   },
   { timestamps: true }
 );
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,12 +3,7 @@
 import { User } from "@/database";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import {
-  GetPushTokenSchema,
-  GetUserSchema,
-  UpdatePushTokenSchema,
-  UpdateUserSchema,
-} from "../validations";
+import { GetUserSchema, UpdateUserSchema } from "../validations";
 import { IUserDoc } from "@/database/user.model";
 
 export async function getUser(
@@ -69,61 +64,6 @@ export async function updateUser(
       success: true,
       data: {
         user: JSON.parse(JSON.stringify(updatedUser)),
-      },
-    };
-  } catch (error) {
-    return handleError(error) as ErrorResponse;
-  }
-}
-
-export async function updatePushToken(
-  params: UpdatePushTokenParams
-): Promise<ActionResponse> {
-  const validationResult = await action({
-    params,
-    schema: UpdatePushTokenSchema,
-  });
-
-  if (validationResult instanceof Error) {
-    return handleError(validationResult) as ErrorResponse;
-  }
-
-  const { userId, token } = validationResult.params!;
-
-  try {
-    await User.findByIdAndUpdate(userId, { pushToken: token });
-
-    return { success: true };
-  } catch (error) {
-    return handleError(error) as ErrorResponse;
-  }
-}
-
-export async function getPushToken(
-  params: GetPushTokenParams
-): Promise<ActionResponse<{ token: string }>> {
-  const validationResult = await action({
-    params,
-    schema: GetPushTokenSchema,
-  });
-
-  if (validationResult instanceof Error) {
-    return handleError(validationResult) as ErrorResponse;
-  }
-
-  const { userId } = validationResult.params!;
-
-  try {
-    const user = await User.findById(userId);
-
-    if (!user || !user.pushToken) {
-      throw new Error("Push token not found for this user");
-    }
-
-    return {
-      success: true,
-      data: {
-        token: user.pushToken,
       },
     };
   } catch (error) {
