63836db45d22db8536835b79120f6acda3552bbe
Le Hoang Lam <lhlam2515@gmail.com>
2025-08-16 00:04:44 +0700

    feat: implement parking session check

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,23 @@ import { auth } from "@/auth";
 import InfoCard from "@/components/cards/InfoCard";
 import SlotForm from "@/components/forms/SlotForm";
 import ToolBar from "@/components/ToolBar";
+import { Button } from "@/components/ui/button";
 import { infoCards } from "@/constants/cards";
+import ROUTES from "@/constants/routes";
+import { getCurrentSession } from "@/lib/actions/session.action";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
 
-  console.log("Session:", session);
+  let isParking = false;
+  if (session && session.user) {
+    const { success, data } = await getCurrentSession({
+      userId: session.user.id as string,
+    });
+
+    isParking = success && Boolean(data?.session);
+  }
 
   return (
     <div className="flex h-full w-full flex-col items-center gap-4">
@@ -39,7 +50,16 @@ export default async function Home() {
       </div>
 
       <ToolBar>
-        <SlotForm />
+        {isParking ? (
+          <Button
+            asChild
+            className="bg-primary-500 text-primary-100 grow p-2 text-base font-bold"
+          >
+            <Link href={ROUTES.PARKING_SESSION}>Go to Parking Session</Link>
+          </Button>
+        ) : (
+          <SlotForm />
+        )}
       </ToolBar>
     </div>
   );
