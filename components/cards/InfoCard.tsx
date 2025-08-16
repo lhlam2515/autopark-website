89a7d63ad83e52438658985398ed0514232649d6
Le Hoang Lam <lhlam2515@gmail.com>
2025-08-16 10:59:12 +0700

    feat: enhance InfoCard to accept custom className and improve content overflow handling

--- components/cards/InfoCard.tsx
+++ components/cards/InfoCard.tsx
@@ -14,11 +14,14 @@ interface Props {
   subtitle?: string;
   imgUrl?: string;
   children?: React.ReactNode;
+  className?: string;
 }
 
-const InfoCard = ({ title, subtitle, imgUrl, children }: Props) => {
+const InfoCard = ({ title, subtitle, imgUrl, children, className }: Props) => {
   return (
-    <Card className="bg-primary-100 flex w-full flex-1 flex-col items-center gap-3 rounded-xl px-1.5 py-2 shadow-md">
+    <Card
+      className={`bg-primary-100 flex w-full flex-1 flex-col items-center gap-3 rounded-xl px-1.5 py-2 shadow-md ${className || ""}`}
+    >
       <CardHeader className="flex w-full flex-col items-start gap-0 px-1">
         <div className="flex items-center gap-2">
           {imgUrl && <Image src={imgUrl} alt={title} width={24} height={24} />}
@@ -34,7 +37,9 @@ const InfoCard = ({ title, subtitle, imgUrl, children }: Props) => {
           )}
         </CardDescription>
       </CardHeader>
-      <CardContent className="h-full w-full px-1">{children}</CardContent>
+      <CardContent className="h-full w-full overflow-y-auto px-1">
+        {children}
+      </CardContent>
     </Card>
   );
 };
