26951c8cd48c932148172bfe9caf8cc8d0f95095
Le Hoang Lam <lhlam2515@gmail.com>
2025-08-17 18:08:50 +0700

    fix: center alignment of the AuthLayout component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="bg-primary-300 flex min-h-screen items-center justify-between px-6 py-10">
-      <section className="bg-primary-100 flex min-w-full flex-col gap-6 rounded-lg px-3 py-6 shadow-md sm:min-w-[520px] sm:px-8">
+    <main className="bg-primary-300 flex min-h-screen items-center justify-center px-4 py-10">
+      <section className="bg-primary-100 flex min-w-full flex-col gap-6 rounded-lg px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
         <div className="flex w-full flex-col items-start">
           <h1 className="text-secondary-500 text-2xl font-semibold">
             Join Auto<span className="text-secondary-100">Park</span>
