d6b0c520cd4a19711be22e8deb1ebf2fc68ced71
Le Hoang Lam <lhlam2515@gmail.com>
2025-08-17 11:43:04 +0700

    fix: refine layout structure and styling for improved responsiveness

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const UserLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="bg-primary-300 relative w-full">
+    <main className="bg-primary-300 relative">
       <Navbar />
 
       <div className="flex">
-        <section className="flex max-h-screen min-h-screen flex-1 flex-col pt-16 pb-16">
-          <div className="h-full w-full">{children}</div>
+        <section className="flex min-h-dvh flex-1 flex-col pt-16 pb-16 sm:min-h-screen sm:px-14">
+          <div className="mx-auto h-full w-full max-w-4xl">{children}</div>
         </section>
       </div>
     </main>
